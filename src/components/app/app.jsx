b55fc9d85891a58fca5b36e587bee3c54b256e57
Sergey Mironov <sergeymironov@protonmail.com>
2022-05-17 00:46:17 +0300

    Initial commit

---
+++ src/components/app/app.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import appStyles from './app.module.css';
+
+const App = () => {
+    return (
+        <div className={appStyles.app}></div>
+    );
+};
+
+export default App;
